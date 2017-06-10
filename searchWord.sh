1855a08519de8ccabbffc547730dd070ea4e4cc7
LE NEVE <lenevejeremy@gmail.com>
2017-06-10 18:06:57 +0200

    search word linear

--- searchWord.sh
+++ searchWord.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 n=$(echo $1 | awk -f /usr/share/awk/ord.awk -f f2.awk)
+echo $n
 
-$file="unsortFr.dic$n"
+#$file="unsortFr.dic$n"
+file="unsortFrTest.dic$n"
 [ ! -r "$file" ] && exit 1
 
+while read word ; do
 
+	IFS=';' read -a myarray <<< "$word"
+	
+	if [ $1 == ${myarray[0]} ]
+	then
+		echo ${myarray[0]}
+		echo ${myarray[1]}
+		break;
+	fi
+done < $file
