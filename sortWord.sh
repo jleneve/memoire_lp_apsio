884bb48a12ac6053a07b48a8f3d41befffe664e3
LE NEVE <lenevejeremy@gmail.com>
2017-06-17 14:57:05 +0200

    tri de fichiers avec ma commande sort

--- sortWord.sh
+++ sortWord.sh
@@ -3,34 +3,5 @@
 file=$1
 [ ! -r "$file" ] && exit 1
 
-./clearFile.sh $file
-
-# size of file
-size=$(wc -l < "$file")
-echo $size
-
-tab=()
-while read word ; do
-	tab+=("$word");
-done < $file
-
-for (( i=0 ; i<size ; i++))
-do
-	for(( j=($i+1) ; j<size ; j++))
-	do
-		lettreI=$(echo ${tab[i]} | awk -f /usr/share/awk/ord.awk -f ordLetterInWord.awk)
-		lettreJ=$(echo ${tab[j]} | awk -f /usr/share/awk/ord.awk -f ordLetterInWord.awk)		
-		if [ $lettreJ -le $lettreI ]
-        	then
-            		tampon=${tab[i]}
-            		tab[i]=${tab[j]}
-            		tab[j]=$tampon
-		fi
-	done
-done
-
-rm $file
-for (( i=0 ; i<size ; i++ ))
-do
-	echo ${tab[i]} >> $file
-done
+sort $file > "tmp_file"
+cat "tmp_file" > $file
